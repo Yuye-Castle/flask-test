0c039d225c111894f9ba04f0939d5540b3b8684c
Yuye Castle <sanxing.9072@gmail.com>
2020-06-19 02:59:04 +0800

    6-19

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,17 @@
 from app import app
 from app.login import LoginForm
 from flask import render_template, flash, redirect
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import logout_user
+from flask_login import login_required
+from app import db
+from app.form import RegistrationForm
+
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username': 'Miguel'}
     posts = [
@@ -21,9 +29,43 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect('/index')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+def url_for(index):
+    if index == 'index':
+        return '/index'
+    if index == 'login':
+        return '/login'
+    if index == 'logout':
+        return '/logout'
+    if index == 'register':
+        return '/register'
--- microblog.py
+++ microblog.py
@@ -1,1 +1,13 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+import os
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True, host='127.0.0.1', port=8888)
+    #app.run
